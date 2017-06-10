7b794e77fdc63e98d10f9328f99a2a5e4d6774b9
Marcel Otto <marcelotto@gmx.de>
2017-06-10 21:50:49 +0200

    core: remove unused exception

--- lib/rdf/exceptions.ex
+++ lib/rdf/exceptions.ex
@@ -46,15 +46,3 @@ end
 defmodule RDF.Namespace.UndefinedTermError do
   defexception [:message]
 end
-
-
-defmodule RDF.InvalidRepoURLError do
-  defexception [:message, :url]
-
-  def exception(opts) do
-    url = Keyword.fetch!(opts, :url)
-    msg = Keyword.fetch!(opts, :message)
-    msg = "invalid url #{url}, #{msg}"
-    %__MODULE__{message: msg, url: url}
-  end
-end
