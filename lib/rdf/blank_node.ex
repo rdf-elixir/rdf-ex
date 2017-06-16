5600f8c1ce4125dad9a581f5cf584b720082bbb1
Marcel Otto <marcelotto@gmx.de>
2017-06-16 13:13:56 +0200

    Add documentation to RDF.BlankNode

--- lib/rdf/blank_node.ex
+++ lib/rdf/blank_node.ex
@@ -10,12 +10,28 @@ defmodule RDF.BlankNode do
 
   @type t :: module
 
+  @doc """
+  Generator function for `RDF.BlankNode`s.
+  """
   def new,
     do: new(make_ref())
+
+  @doc """
+  Generator function for `RDF.BlankNode`s with a user-defined identity.
+
+  ## Examples
+
+      iex> RDF.bnode(:foo)
+      %RDF.BlankNode{id: "foo"}
+  """
+  def new(id)
+
   def new(id) when is_binary(id),
     do: %RDF.BlankNode{id: id}
+
   def new(id) when is_reference(id),
     do: id |> :erlang.ref_to_list |> to_string |> String.replace(~r/\<|\>/, "") |> new
+
   def new(id) when is_atom(id) or is_integer(id),
     do: id |> to_string |> new
 end
