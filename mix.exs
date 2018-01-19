65bbd78953d1a8dcbdf85e92f5f6cbee2f9309a4
Marcel Otto <marcelotto@gmx.de>
2018-01-19 22:12:47 +0100

    Add CHANGELOG.md to package files

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule RDF.Mixfile do
       maintainers: ["Marcel Otto"],
       licenses: ["MIT"],
       links: %{"GitHub" => @repo_url},
-      files: ~w[lib src priv mix.exs README.md LICENSE.md VERSION]
+      files: ~w[lib src priv mix.exs README.md CHANGELOG.md LICENSE.md VERSION]
     ]
   end
 
