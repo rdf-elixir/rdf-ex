8e1dc13c4fb76cf87c5001633f1b7e7b265e9958
Marcel Otto <marcelotto@gmx.de>
2018-06-13 21:56:12 +0200

    Remove mix_test_watch config

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,1 @@
 use Mix.Config
-
-unless Mix.env in ~w[docs bench]a do
-  import_config "#{Mix.env}.exs"
-end
--- config/dev.exs
+++
@@ -1,9 +0,0 @@
-use Mix.Config
-
-config :mix_test_watch,
-  clear: true,
-  tasks: [
-    "test",
-#    "dialyzer",
-#    "credo",
-  ]
