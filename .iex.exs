c4cbae3525685ee583eb9a9e286f4e10202377e8
Marcel Otto <marcelotto@gmx.de>
2018-06-14 02:06:37 +0200

    Add .iex.exs file

---
+++ .iex.exs
@@ -0,0 +1,22 @@
+import RDF.Sigils
+
+alias RDF.NS.{XSD, RDFS, OWL, SKOS}
+
+alias RDF.{
+  Term,
+  IRI,
+  Literal,
+  BlankNode,
+
+  Triple,
+  Quad,
+  Statement,
+
+  Description,
+  Graph,
+  Dataset,
+}
+
+alias RDF.BlankNode, as: BNode
+
+alias RDF.{NTriples, NQuads, Turtle}
