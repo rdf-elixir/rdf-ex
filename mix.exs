874b0d5eea85abcae69929db04f2c0a45025c337
Marcel Otto <marcelotto@gmx.de>
2019-06-09 21:48:03 +0200

    Include test support files via elixirc_paths

--- mix.exs
+++ mix.exs
@@ -13,6 +13,7 @@ defmodule RDF.Mixfile do
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env()),
 
       # Hex
       package: package(),
@@ -75,4 +76,7 @@ defmodule RDF.Mixfile do
       {:erlang_term, "~> 1.7",    only: :bench},
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,7 +1,1 @@
 ExUnit.start()
-
-with {:ok, files} = File.ls("./test/support") do
-  Enum.each files, fn(file) ->
-    Code.require_file "support/#{file}", __DIR__
-  end
-end
