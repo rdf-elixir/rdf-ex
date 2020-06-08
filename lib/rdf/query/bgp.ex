1ca082cbb2f566f99187327b75e5caa410e9ac5c
Marcel Otto <marcelotto@gmx.de>
2020-06-08 10:08:09 +0200

    Add and improve existing typespecs on BGP behaviour

--- lib/rdf/query/bgp.ex
+++ lib/rdf/query/bgp.ex
@@ -1,13 +1,21 @@
 defmodule RDF.Query.BGP do
   @moduledoc """
-  An interface for various BGP algorithm implementations.
+  An interface for various BGP matching algorithm implementations.
   """
 
+  @type variable :: String.t
+  @type triple_pattern :: {
+                            subject :: variable | RDF.Term.t,
+                            predicate :: variable | RDF.Term.t,
+                            object :: variable | RDF.Term.t
+                          }
+  @type triple_patterns :: list(triple_pattern)
   @type solution :: map
   @type solutions :: [solution]
 
-  @callback query(triple_patterns :: [], data :: RDF.Data.t, opts :: Keyword.t) :: solutions
 
-  @callback query_stream(triple_patterns :: [], data :: RDF.Data.t, opts :: Keyword.t) :: Enumerable.t()
+  @callback query(triple_patterns :: [], data :: RDF.Graph.t, opts :: Keyword.t) :: solutions
+
+  @callback query_stream(triple_patterns :: [], data :: RDF.Graph.t, opts :: Keyword.t) :: Enumerable.t()
 
 end
