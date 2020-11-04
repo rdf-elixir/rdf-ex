25b72398431201c8facd3b4c7ff9c7d553cec82d
Marcel Otto <marcelotto@gmx.de>
2020-11-04 16:44:53 +0100

    Add RDF.NQuads.Decoder.decode_from_stream/2

--- lib/rdf/serializations/nquads_decoder.ex
+++ lib/rdf/serializations/nquads_decoder.ex
@@ -9,18 +9,39 @@ defmodule RDF.NQuads.Decoder do
 
   @impl RDF.Serialization.Decoder
   @spec decode(String.t(), keyword) :: {:ok, Dataset.t()} | {:error, any}
-  def decode(content, _opts \\ []) do
-    with {:ok, tokens, _} <- tokenize(content),
-         {:ok, ast} <- parse(tokens) do
+  def decode(string, _opts \\ []) do
+    with {:ok, ast} <- do_decode(string, true) do
       {:ok, build_dataset(ast)}
+    end
+  end
+
+  @impl RDF.Serialization.Decoder
+  @spec decode_from_stream(Enumerable.t(), keyword) :: Dataset.t()
+  def decode_from_stream(stream, _opts \\ []) do
+    Enum.reduce(stream, Dataset.new(), fn line, dataset ->
+      case do_decode(line, false) do
+        {:ok, []} -> dataset
+        {:ok, [[quad]]} -> Dataset.add(dataset, quad)
+        {:error, error} -> raise error
+      end
+    end)
+  end
+
+  defp do_decode(content, error_with_line_number) do
+    with {:ok, tokens, _} <- tokenize(content) do
+      parse(tokens)
     else
       {:error, {error_line, :ntriples_lexer, error_descriptor}, _error_line_again} ->
         {:error,
-         "N-Quad scanner error on line #{error_line}: #{error_description(error_descriptor)}"}
+         "N-Quad scanner error#{if error_with_line_number, do: " on line #{error_line}"}: #{
+           error_description(error_descriptor)
+         }"}
 
       {:error, {error_line, :nquads_parser, error_descriptor}} ->
         {:error,
-         "N-Quad parser error on line #{error_line}: #{error_description(error_descriptor)}"}
+         "N-Quad parser error#{if error_with_line_number, do: " on line #{error_line}"}: #{
+           error_description(error_descriptor)
+         }"}
     end
   end
 
@@ -28,7 +49,9 @@ defmodule RDF.NQuads.Decoder do
 
   defp parse(tokens), do: tokens |> :nquads_parser.parse()
 
-  defp build_dataset(ast) do
-    Enum.reduce(ast, Dataset.new(), &Dataset.add(&2, &1))
+  defp build_dataset([]), do: Dataset.new()
+
+  defp build_dataset([quads]) do
+    Enum.reduce(quads, Dataset.new(), &Dataset.add(&2, &1))
   end
 end
