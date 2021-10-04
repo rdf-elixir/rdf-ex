c3556bf79ed875ed14a2ea237489b4e117995f45
Marcel Otto <marcelotto@gmx.de>
2021-10-05 01:09:29 +0200

    Add tests for nested quoted triples in N-Triples-star

--- test/unit/ntriples_star_encoder_test.exs
+++ test/unit/ntriples_star_encoder_test.exs
@@ -3,14 +3,14 @@ defmodule RDF.Star.NTriples.EncoderTest do
 
   alias RDF.NTriples
 
-  test "annotations of triples on subject position" do
+  test "quoted triples on subject position" do
     assert NTriples.Encoder.encode!(graph_with_annotation()) ==
              """
              << <http://example.com/S> <http://example.com/P> "Foo" >> <http://example.com/ap> <http://example.com/ao> .
              """
   end
 
-  test "annotations of triples on object position" do
+  test "quoted triples on object position" do
     assert NTriples.Encoder.encode!(
              Graph.new([
                statement(),
@@ -20,6 +20,21 @@ defmodule RDF.Star.NTriples.EncoderTest do
              """
              <http://example.com/AS> <http://example.com/ap> << <http://example.com/S> <http://example.com/P> "Foo" >> .
              <http://example.com/S> <http://example.com/P> "Foo" .
+             """
+  end
+
+  test "nested quoted triples" do
+    assert NTriples.Encoder.encode!(
+             Graph.new([
+               {
+                 {{EX.s1(), EX.p1(), EX.o1()}, EX.q1(), {EX.s2(), EX.p2(), EX.o2()}},
+                 EX.q2(),
+                 {{EX.s3(), EX.p3(), EX.o3()}, EX.q3(), {EX.s4(), EX.p4(), EX.o4()}}
+               }
+             ])
+           ) ==
+             """
+             << << <http://example.com/s1> <http://example.com/p1> <http://example.com/o1> >> <http://example.com/q1> << <http://example.com/s2> <http://example.com/p2> <http://example.com/o2> >> >> <http://example.com/q2> << << <http://example.com/s3> <http://example.com/p3> <http://example.com/o3> >> <http://example.com/q3> << <http://example.com/s4> <http://example.com/p4> <http://example.com/o4> >> >> .
              """
   end
 end
