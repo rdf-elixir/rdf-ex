b38a57a27a9338fb9c7abd40f10b212381252cc5
Marcel Otto <marcelotto@gmx.de>
2021-12-13 11:43:07 +0100

    Upgrade dependencies

--- mix.exs
+++ mix.exs
@@ -70,9 +70,9 @@ defmodule RDF.Mixfile do
     [
       {:decimal, "~> 1.5"},
       {:protocol_ex, "~> 0.4.4"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.26", only: :dev, runtime: false},
       {:excoveralls, "~> 0.14", only: :test},
       {:benchee, "~> 1.0", only: :bench}
     ]
