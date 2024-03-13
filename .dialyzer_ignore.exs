3bc4c376168fff66f3ba0197de9cc3258707ef23
Marcel Otto <marcelotto@gmx.de>
2024-03-13 20:34:11 +0100

    Ignore dialyzer warnings in EarlFormatter

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,3 +1,5 @@
 [
-  {"lib/rdf/vocabulary/namespace/vocabulary_namespace.ex", :call}
+  {"lib/rdf/vocabulary/namespace/vocabulary_namespace.ex", :call},
+  {"lib/rdf/earl_formatter.ex", :call_to_missing},
+  {"lib/rdf/earl_formatter.ex", :unknown_function}
 ]
